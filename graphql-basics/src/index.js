0cc81ef4693152fbc3edc7401933f4c4600ff119
Ba-Zen <bazenatlas@gmail.com>
2019-12-11 17:24:16 -0500

    import exercises

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello GraphQL');
+import myCurrentLocation, { getGreeting, message, name } from './myModule';
+import add, { subtract } from './math';
+
+console.log(message);
+console.log(name);
+console.log(myCurrentLocation);
+console.log(getGreeting('Bobby'));
+
+console.log(add(2, 4));
+console.log(subtract(10, 3));
