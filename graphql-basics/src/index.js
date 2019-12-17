931aebf55cb3b0c3facd4c31b3a32e288342e241
Ba-Zen <bazenatlas@gmail.com>
2019-12-17 11:41:06 -0500

    adds babel object spread operator package, speading in args

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -175,9 +175,7 @@ const resolvers = {
 
       const user = {
         id: uuidv4(),
-        name: args.name,
-        email: args.email,
-        age: args.age
+        ...args
       };
 
       users.push(user);
@@ -193,10 +191,7 @@ const resolvers = {
 
       const post = {
         id: uuidv4(),
-        title: args.title,
-        body: args.body,
-        published: args.published,
-        author: args.author
+        ...args
       };
 
       posts.push(post);
@@ -215,9 +210,7 @@ const resolvers = {
 
       const comment = {
         id: uuidv4(),
-        text: args.text,
-        author: args.author,
-        post: args.post
+        ...args
       };
 
       comments.push(comment);
